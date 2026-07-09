742f5a97f2d05104459ecfdfa5c81716715664c1
sunshinesDL <sevenzard@163.com>
2026-07-09 17:09:37 +0800

    add chapter3.1's practices

---
+++ codes/chapter_03/prac_3_1_print_list.py
@@ -0,0 +1,10 @@
+# Created Time: 2026/07/09
+# Author: sunshinesDL (sunshinesDL@163.com)
+
+"""Print list elements."""
+
+
+cats = ['Mimi', 'Xiaomi']
+
+for cat in cats:
+    print(cat)
---
+++ codes/chapter_03/prac_3_2_greet_cats.py
@@ -0,0 +1,10 @@
+# Created Time: 2026/07/09
+# Author: sunshinesDL (sunshinesDL@163.com)
+
+"""Greet cats in the list."""
+
+
+cats = ['Mimi', 'Xiaomi']
+
+for cat in cats:
+    print(f"{cat}, How are you?")
